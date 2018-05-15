186fcb3435c6c827305d660c22c7de3186c62c2f
Tatevik Sharyan <shtatev4991@gmail.com>
2018-05-15 09:32:21 +0400

    First point of assignment modified. Stored procedure execution command does not work!

--- SQLQuery12.sql
+++ SQLQuery12.sql
@@ -2,13 +2,8 @@ SELECT *
 FROM Production.Product;
 
 
-SELECT [Name], ProductNumber, ListPrice 
+SELECT [Name], ProductNumber, ListPrice as Price
 FROM Production.Product
-
-
-GO
-EXEC sp_RENAME 'Production.Product.ListPrice', 'Price', 'COLUMN'
-GO
 
 
 SELECT *
@@ -42,21 +37,25 @@ FROM Sales.SalesOrderDetail
 GROUP BY ProductID
 HAVING AVG(OrderQty) <= 4
 
-
+GO  
+IF OBJECT_ID('dbo.uspGetEmployeeManagersPerDepartment', 'P') IS NOT NULL  
+   DROP PROCEDURE dbo.uspGetEmployeeManagersPerDepartment;
 GO
 CREATE PROCEDURE uspGetEmployeeManagersPerDepartment
 	@BusinessEntityID INTEGER,
 	@Department VARCHAR	
 AS
 SELECT employee.BusinessEntityID, employee.FirstName, employee.LastName, employee.JobTitle, 
-	manager.Gender, manager.FirstName, manager.LastName
+	manager.Gender as ManagerGender, manager.FirstName as ManagerFirstName, manager.LastName as ManagerLastName
 FROM
 	(SELECT p.BusinessEntityID, p.FirstName, p.LastName, e.JobTitle, d.DepartmentID
 		FROM Person.Person p
 			inner join HumanResources.EmployeeDepartmentHistory h ON p.BusinessEntityID = h.BusinessEntityID
 			inner join HumanResources.Department d ON h.DepartmentID = d.DepartmentID
 			inner join HumanResources.Employee e ON e.BusinessEntityID = p.BusinessEntityID
-		WHERE p.BusinessEntityID = @BusinessEntityID and d.[Name] = @Department) employee
+		WHERE p.BusinessEntityID = @BusinessEntityID
+		 and d.[Name] = @Department
+		 ) employee
 		right join
 	(SELECT e.Gender, p.FirstName, p.LastName, d.DepartmentID
 		FROM HumanResources.Department d
@@ -66,10 +65,33 @@ FROM
 			ON h.BusinessEntityID = e.BusinessEntityID
 			inner join Person.Person p
 			ON p.BusinessEntityID = h.BusinessEntityID
-		WHERE d.[Name] = 'Engineering'--@Department
+		WHERE d.[Name] = @Department
 		 and e.JobTitle like '%Manager%') manager
 	ON employee.DepartmentID = manager.DepartmentID
+
+RETURN
 GO
 
-
+GO
 EXEC dbo.uspGetEmployeeManagersPerDepartment @BusinessEntityID = 2, @Department = 'Engineering';
+
+select *
+from 
+(SELECT p.BusinessEntityID, p.FirstName, p.LastName, e.JobTitle, d.DepartmentID
+		FROM Person.Person p
+			inner join HumanResources.EmployeeDepartmentHistory h ON p.BusinessEntityID = h.BusinessEntityID
+			inner join HumanResources.Department d ON h.DepartmentID = d.DepartmentID
+			inner join HumanResources.Employee e ON e.BusinessEntityID = p.BusinessEntityID
+		WHERE p.BusinessEntityID = 2 and d.[Name] = 'Engineering') employe
+		right join
+(SELECT e.Gender, p.FirstName, p.LastName, d.DepartmentID
+		FROM HumanResources.Department d
+			inner join HumanResources.EmployeeDepartmentHistory h 
+			ON d.DepartmentID = h.DepartmentID
+			inner join HumanResources.Employee e
+			ON h.BusinessEntityID = e.BusinessEntityID
+			inner join Person.Person p
+			ON p.BusinessEntityID = h.BusinessEntityID
+		WHERE d.[Name] = 'Engineering'
+		 and e.JobTitle like '%Manager%') dep
+		 on employe.DepartmentID = dep.DepartmentID
